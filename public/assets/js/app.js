aeaf38c7956e42499a2dbae053b8066d17ce414d
Guilherme Araújo <guilherme.moreira586@outlook.com>
2018-06-07 20:26:22 -0300

    Atualização 07/06

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log("Acessou o arquivo App.js");
+function CadastrarDespesa(){
+    var description = document.getElementById("description").value;
+    var amount = document.getElementById("amount").value;
+    var dateAdd = document.getElementById("dateAdd").value;
+    
+    console.log("Descrição:",description,"\nValor Total:",amount,"\nData:",dateAdd);
+
+}
